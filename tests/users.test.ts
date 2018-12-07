47a08f49fdebea3083bb88f2aecd3a66797a8465
Eduardo Matos <eduardo.matos@redealumni.com>
2018-12-07 18:16:29 -0200

    Implement all tests

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -15,48 +15,60 @@ describe('Read', () => {
     expect(res.body).to.eql({ success: true, user: { id: user.id, name: user.name } });
   });
 
-  it.skip('Get all users', async () => {
+  it('Get all users', async () => {
     // :: Criar 2 usuários, um com nome 'Spam' e outro com nome 'Fish' :: //
-
+    const [s, f] = await Promise.all([
+      User.create({ name: 'Spam' }),
+      User.create({ name: 'Fish' }),
+    ]);
     const res = await client().get('/users');
 
     expect(res).to.have.status(OK);
-    expect(res.body).to.eql({ success: true, users: [{ id: 1, name: 'Spam' }, { id: 2, name: 'Fish' }] });
+    expect(res.body).to.eql({ success: true, users: [{ id: s.id, name: 'Spam' }, { id: f.id, name: 'Fish' }] });
   });
 
-  it.skip('Return not found if user does not exist');
+  it('Return not found if user does not exist', async () => {
+    const result = await client().get('/users/3');
+
+    expect(result).to.have.status(NOT_FOUND);
+    expect(result.body).to.eql({success: false});
+  });
 });
 
 describe('Create', () => {
-  it.skip('Create user', () => {
+  it('Create user', async () => {
     // :: Converter para async/await :: //
-
-    return client().post('/users').send({ name: 'Foo' }).then((res) => {
-      return User.findOne({ where: { name: 'Foo' } }).then((user: any) => {
-        expect(res.body).to.eql({ success: true, user: { id: user.id, name: 'Foo' } });
-        expect(res).to.have.status(CREATED);
-      });
-    });
+    const res = await client().post('/users').send({ name: 'Foo' });
+    const user: any = await User.findOne({ where: { name: 'Foo' } });
+    expect(res.body).to.eql({ success: true, user: { id: user.id, name: 'Foo' } });
+    expect(res).to.have.status(CREATED);
   });
 });
 
 describe('Delete', () => {
-  it.skip('Delete single user', async () => {
+  it('Delete single user', async () => {
     // :: Converter para async/await :: //
 
-    return User.create({ name: 'Yay' }).then((user: any) => {
-      return client().del(`/users/${user.id}`).then((res) => {
-        expect(res).to.have.status(NO_CONTENT);
-        expect(res.text).to.equal('');
-
-        return User.findAll().then((users: any) => {
-          expect(users).to.eql([]);
-        });
-      });
-    });
+    const user = await User.create({ name: 'Yay' });
+    const res = await client().del(`/users/${user.id}`);
+    expect(res).to.have.status(NO_CONTENT);
+    expect(res.text).to.equal('');
+    const users = await User.findAll();
+    expect(users).to.eql([]);
   });
 
-  it.skip('Delete all users');
+  it('Delete all users', async () => {
+    await Promise.all([
+      User.create({name: 'baz'}),
+      User.create({name: 'bar'}),
+    ]);
+    let users = await User.findAll();
+    expect(users.length).to.eql(2);
+    const res = await client().del('/users');
+    users = await User.findAll();
+    expect(users.length).to.eql(0);
+    expect(res).to.have.status(NO_CONTENT);
+  });
 
   it('Returns not found if user does not exist', async () => {
     const res = await client().del('/users/99');
@@ -67,6 +79,20 @@ describe('Delete', () => {
 });
 
 describe('Update', () => {
-  it.skip('Updates user');
-  it.skip('Returns not found if user does not exist');
+  it('Updates user', async () => {
+    let user = await User.create({name: 'baz'});
+    const res = await client().put(`/users/${user.id}`).send({ name: 'Foo' });
+
+    user = await User.findOne({where:{id:user.id}});
+
+    expect(res).to.have.status(OK);
+    expect(user.name).to.eq('Foo');
+    expect(res.body).to.eql({ success: true, user: { id: user.id, name: 'Foo' } });
+  });
+  it('Returns not found if user does not exist', async () => {
+    const res = await client().put('/users/1').send({ name: 'Foo' });
+
+    expect(res).to.have.status(NOT_FOUND);
+    expect(res.body).to.eql({ success: false });
+  });
 });
